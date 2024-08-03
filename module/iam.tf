19c493148bc26a6d862ee1b11d064fdf04b13837
Aman Pathak <85124250+AmanPathak-DevOps@users.noreply.github.com>
2024-08-03 19:28:40 +0530

    Update iam.tf

--- module/iam.tf
+++ module/iam.tf
@@ -64,7 +64,7 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonEC2ContainerRegistryReadOnl
 
 resource "aws_iam_role_policy_attachment" "eks-AmazonEBSCSIDriverPolicy" {
   count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.eks-nodegroup-role[count.index].name
 }
 
