ea6255f528ba77fd11db6864ffe8726a8231920d
Pratik Mahalle <124587957+pratik-mahalle@users.noreply.github.com>
2024-11-28 21:55:38 +0530

    Update backend.tf
    Jenkins pipeline was failing due to incorrect region

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "my-ews-baket1122"
-    region         = "us-east-2"
+    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
