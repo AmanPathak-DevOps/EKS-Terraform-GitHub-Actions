8807c15141a21a1ff652812ffd86afbb91be94ed
Aman Pathak <85124250+AmanPathak-DevOps@users.noreply.github.com>
2025-01-05 11:25:04 +0530

    Updated az for eks cluster

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "dev-aman-tf-bucket"
-    region         = "us-east-2"
+    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
