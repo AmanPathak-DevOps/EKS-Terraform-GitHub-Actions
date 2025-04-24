c4c2317a0f3a99fa25f8c1ad49c1836c2ff10d9b
oezeakachi <oezeakac@redhat.com>
2025-04-24 17:15:10 +0100

    Added Changes

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "dev-aman-tf-bucket"
-    region         = "us-east-1"
+    region         = "eu-west-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
