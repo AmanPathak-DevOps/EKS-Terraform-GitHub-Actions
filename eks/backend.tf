7e74e54571b44cb35a6856b0fbdb6727763f3c7d
iam-srihari <srihari.mulakalapalli@gmail.com>
2025-04-30 00:50:02 +0530

    added bucket

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "murali25-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
