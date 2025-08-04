2b24f7480fd0e045816e90676a41b7445de084b3
Chethan-tr <chethantr6@gmail.com>
2025-08-04 12:42:31 +0530

    Update backend.tf
    updated bucket

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "chethan-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
